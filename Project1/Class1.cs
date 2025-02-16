1d50a97399efd5b6ce39d516a13cbc89ef4ffd3f
XavierC003 <xaviercollege3@gmail.com>
2025-02-16 18:02:08 -0500

    Base class for nodes and leaf node with a node that holds rectangle

--- Project1/Class1.cs
+++ Project1/Class1.cs
@@ -1,6 +1,22 @@
 ﻿namespace Project1;
 
-public class Class1
-{
+using Systen;
+using System.Collections.Generic;
 
+// Base class for quadtree nodes. Stores all the common properties
+public abstract class Node
+{
+    public int x { get; set; }  // X-coordinate of the node
+    public int y { get; set: }  // Y-coordinate of the node
+    public int Width { get; set; }  // Width of the node's area
+    public int Height { get; set; }  // Height of the node's area
 }
+
+// Leaf node class representing a node that holds rectangles.
+public class LeafNode : Node
+{
+    // List of rectangles contained in the leaf node
+    public List<Rectangle> Rectangles { get; set; } = new List<Rectangle>();
+    
+}
+
