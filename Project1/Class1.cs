0c4651d6c8b1dc4d5ee75d06d82525bad7e3c66b
XavierC003 <xaviercollege3@gmail.com>
2025-02-19 00:10:48 -0500

    Constructor to initialize a rectangle with given values

--- Project1/Class1.cs
+++ Project1/Class1.cs
@@ -1,13 +1,13 @@
 ﻿namespace Project1;
 
-using Systen;
+using System;
 using System.Collections.Generic;
 
 // Base class for quadtree nodes. Stores all the common properties
 public abstract class Node
 {
-    public int x { get; set; }  // X-coordinate of the node
-    public int y { get; set: }  // Y-coordinate of the node
+    public int X { get; set; }  // X-coordinate of the node
+    public int Y { get; set; }  // Y-coordinate of the node
     public int Width { get; set; }  // Width of the node's area
     public int Height { get; set; }  // Height of the node's area
 }
@@ -29,9 +29,17 @@ public class InternalNode : Node
 
 // Class representing a rectangle with position and dimensions
 public class Rectangle{
-    public int x { get; set; } // X=coordinate of the bottom left 
-    public int y { get; set; } // Y-Coordinate of the bottom left
+    public int X { get; set; } // X=coordinate of the bottom left 
+    public int Y { get; set; } // Y-Coordinate of the bottom left
     public int Width { get; set; } // Width of the rectangle
     public int Height { get; set; } // Height of the rectangle
+    // Constructor to initialize a rectangle with given values
+public Rectangle(int x, int y, int width, int height)
+{
+    X = x;
+    Y = y;
+    Width = width;
+    Height = height;
 }
 
+}
