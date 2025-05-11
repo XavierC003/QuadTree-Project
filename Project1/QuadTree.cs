ee635b127bcc92800af228ad2ba0a3f3e2498cf4
XavierC003 <xaviercollege3@gmail.com>
2025-05-10 20:45:16 -0400

    Added documentation to QuadTree.cs

--- Project1/QuadTree.cs
+++ Project1/QuadTree.cs
@@ -9,30 +9,57 @@ public class Quadtree
     {
         private Node root;
 
+
+        /// <summary>
+        /// Initializes the quadtree with a 100x100 root leaf node.
+        /// </summary>
         public Quadtree()
         {
             root = new LeafNode(-50, -50, 100, 100);
         }
 
+        /// <summary>
+        /// Inserts a rectangle into the quadtree.
+        /// </summary>
+        /// <param name="x">X coordinate of the rectangle</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="width">Width of the rectangle</param>
+        /// <param name="height">Height of the rectangle</param>
         public void Insert(int x, int y, int width, int height)
         {
             root = root.Insert(x, y, width, height);
         }
 
+        /// <summary>
+        /// Deletes a rectangle at the specified coordinates.
+        /// </summary>
         public void Delete(int x, int y)
         {
             root = root.Delete(x, y);
         }
+
+
+        /// <summary>
+        /// Finds and displays a rectangle at the specified location.
+        /// </summary>
 
         public void Find(int x, int y)
         {
             Console.WriteLine(root.Find(x, y));
         }
 
+
+        /// <summary>
+        /// Updates the dimensions of a rectangle at the given coordinates.
+        /// </summary>
         public void Update(int x, int y, int width, int height)
         {
             root = root.Update(x, y, width, height);
         }
+
+        /// <summary>
+        /// Prints the entire quadtree hierarchy.
+        /// </summary>
 
         public void Dump()
         {
